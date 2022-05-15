5724d0271ea6f2e18eb91656158ee375868d65e2
zq <1846570844@qq.com>
2022-05-15 21:18:22 +0800

    添加文件

---
+++ head/Softmax.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Parameter
+import math
+
+
+class Softmax(nn.Module):
+    r"""Implement of Softmax (normal classification head):
+        Args:
+            in_features: size of each input sample
+            out_features: size of each output sample
+            device_id: the ID of GPU where the model will be trained by model parallel.
+                       if device_id=None, it will be trained on CPU without model parallel.
+        """
+    def __init__(self, in_features, out_features, device_id):
+        super(Softmax, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.device_id = device_id
+
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        self.bias = Parameter(torch.FloatTensor(out_features))
+        nn.init.xavier_uniform_(self.weight)
+        nn.init.zero_(self.bias)
+
+    def forward(self, x):
+        if self.device_id == None:
+            out = F.linear(x, self.weight, self.bias)
+        else:
+            sub_weights = torch.chunk(self.weight, len(self.device_id), dim=0)
+            sub_biases = torch.chunk(self.bias, len(self.device_id), dim=0)
+            temp_x = x.cuda(self.device_id[0])
+            weight = sub_weights[0].cuda(self.device_id[0])
+            bias = sub_biases[0].cuda(self.device_id[0])
+            out = F.linear(temp_x, weight, bias)
+            for i in range(1, len(self.device_id)):
+                temp_x = x.cuda(self.device_id[i])
+                weight = sub_weights[i].cuda(self.device_id[i])
+                bias = sub_biases[i].cuda(self.device_id[i])
+                out = torch.cat((out, F.linear(temp_x, weight, bias).cuda(self.device_id[0])), dim=1)
+        return out
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
